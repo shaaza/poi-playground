c90864317eacef189822f334b88b5f774076d7a7
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-08-11 12:05:33 +0700

    Setup containers to hold search panels.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Search from './containers/Search/'
+
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app-panel">
+        <Search />
+        <Search />
       </div>
     );
   }
---
+++ src/containers/Search/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import './index.css'
+
+class Search extends Component {
+  render() {
+    return (
+      <div className="search-container">
+        <div>
+          <h3>Foursquare</h3>
+        </div>
+        <p>
+            Address bar
+        </p>
+        <p>
+            Search results
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Search;
