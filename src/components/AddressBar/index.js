4c8696ebe69f921a6c1bb74abf441296122948be
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-08-11 12:33:58 +0700

    Add value mgmt for url field.

--- src/components/AddressBar/index.js
+++ src/components/AddressBar/index.js
@@ -5,7 +5,7 @@ function AddressBar(props) {
     return (
       <div className="address-bar">
         <label>URL: </label>
-        <input type="text"></input>
+        <input type="text" value={props.url} onChange={props.onChange}></input>
       </div>
     );
 }
--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -6,20 +6,26 @@ class Search extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      title: props.title
+      title: props.title,
+      url: ""
     } 
   }
+
+  handleURLChange = (event) => {
+    this.setState({url: event.target.value});
+  }
+
   render() {
     return (
       <div className="search-container">
         <div>
           <h3>{this.state.title}</h3>
         </div>
+        <div>
+          <AddressBar url={this.state.url} onChange={this.handleURLChange} />
+        </div>
         <p>
-            <AddressBar props={this.url}/>
-        </p>
-        <p>
-            Search results
+          Search results
         </p>
       </div>
     );
